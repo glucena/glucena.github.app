19234786abab563a7da0f08b7835c8dee54b771c
glucena <gabriellucenatoro@gmail.com>
2018-01-05 12:46:08 +0100

    Adding content to detailed view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgsRevealConfig } from 'ng-scrollreveal';
+import { ActivatedRoute, Router } from '@angular/router';
+import {LocationStrategy} from '@angular/common'
 
 @Component({
   selector: 'app-root',
@@ -13,12 +15,20 @@ export class AppComponent {
   me = 'Gabriel Lucena Toro';
   title = 'FullStack Developer';
 
-  constructor(config: NgsRevealConfig) {
+  constructor(
+    config: NgsRevealConfig,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
     // customize default values of ng-scrollreveal directives used by this component tree
     config.duration = 1000;
     //config.rotate = { x: 10, y: 90, z: 0 };
     config.easing = 'cubic-bezier(0.800, 0.300, 0.300, 0.800)';
 
     //other options here
+  }
+
+  isHidden () {
+    return this.router.url === '/detailed-cv';
   }
 }
